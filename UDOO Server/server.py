c5e4a3d414ecd404450fbb3d0a490d56fa940785
quynhhgoogoo <luongdiemquynh1998@gmail.com>
2019-12-03 11:09:23 +0200

    specification version 1

--- UDOO Server/server.py
+++ UDOO Server/server.py
@@ -1,8 +1,10 @@
 import os
 import json
-from flask import Flask, request, render_template
+from flask import Flask,render_template,request,redirect,url_for
 
 app = Flask(__name__)
+
+#conn = MySQLdb.connect(host="mysql.cc.puv.fi",user="e1601128",password="",db="Vn28YXTwjmPk")
 
 #Rendering web template
 #@app.route('/')                                 
@@ -15,6 +17,7 @@ def userdata():
     with open("test.json", "r") as json_data:
         data = json.load(json_data)
     return render_template("index.html", page_title="User data", test = data)
+
 
 if __name__ == '__main__':
     app.run()
